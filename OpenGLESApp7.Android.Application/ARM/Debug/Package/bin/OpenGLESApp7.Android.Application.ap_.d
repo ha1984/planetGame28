7de12ed5b97afa85fee2f3c7184aa0caad2db69b
ha1984 <henningahrens@hotmail.de>
2017-03-16 19:09:23 +0100

    Add files via upload

---
+++ OpenGLESApp7.Android.Application/ARM/Debug/Package/bin/OpenGLESApp7.Android.Application.ap_.d
@@ -0,0 +1,15 @@
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\bin\OpenGLESApp7.Android.Application.ap_ \
+ : C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\res\values\strings.xml \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\Astronaut.png \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\ground.png \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\sky2.png \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\sky2.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step1.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step2.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step3.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step4.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step5.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step6.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step7.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\assets\step8.txt \
+C:\Users\Ahrens\Documents\VisualStudio\Projects\OpenGLESApp7\OpenGLESApp7\OpenGLESApp7.Android.Application\ARM\Debug\Package\bin\AndroidManifest.xml \
